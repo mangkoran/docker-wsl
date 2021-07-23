0c2949fa2e3069a5fb13584d905e6232e6833b3e
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-07-23 20:32:42 +0800

    Fix DOCKER_DIR's mkdir permission argument

--- docker-service.sh
+++ docker-service.sh
@@ -40,7 +40,7 @@ DOCKER_DIR=/mnt/wsl/shared-docker
 DOCKER_SOCK="$DOCKER_DIR/docker.sock"
 export DOCKER_HOST="unix://$DOCKER_SOCK"
 if [ ! -S "$DOCKER_SOCK" ]; then
-    mkdir -pm o=,ug=rw "$DOCKER_DIR"
+    mkdir -pm o=,ug=rwx "$DOCKER_DIR"
     chgrp docker "$DOCKER_DIR"
     /mnt/c/Windows/System32/wsl.exe $DOCKER_DISTRO sh -c "nohup sudo -b dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1"
 fi
